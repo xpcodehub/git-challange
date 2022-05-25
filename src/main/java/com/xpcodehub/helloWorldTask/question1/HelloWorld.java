583d801b46a8eabb44e8f1268aa21657fd636947
GustavoSTZ <stzgustavo@gmail.com>
2022-05-25 02:07:18 -0300

    simple git challange

---
+++ src/main/java/com/xpcodehub/helloWorldTask/question1/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.xpcodehub.helloWorldTask.question1;
+
+public class HelloWorld {
+
+/*
+* TODO Deve-se alterar o texto para "Hello World" para mantermos a tradição do mundo de desenvolvimento da primera tarefa sempre envolver tal frase :).
+*  Ps: Não esqueça de alterar para o seu usuário no arquivo user.properties da pasta resources.
+* */
+
+    public String sendHelloWorld() {
+        return "Corrigir aqui";
+    }
+}
